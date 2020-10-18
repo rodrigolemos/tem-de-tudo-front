e8a996149cb9d8ff63efcc92c68563e86b23c97c
Rodrigo <rodrigolemosrl@hotmail.com>
2020-10-18 20:39:48 -0300

    Customizing loading message

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import ReactLoading from 'react-loading';
+
+import { Container } from './styles';
+import { colors } from '../../styles/global';
 
 const Loading = () => {
   return (
-    <div>Carregando...</div>
+    <Container>
+      <ReactLoading type={"spin"} color={colors.bgPrimary} height={"10%"} width={"10%"}/>
+    </Container>
   )
 }
 
