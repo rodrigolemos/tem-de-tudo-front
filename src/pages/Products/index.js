bddc2eeb39027aa1a731d64df8b8da554d9be7d3
Rodrigo <rodrigolemosrl@hotmail.com>
2020-10-18 14:26:24 -0300

    CHECKPOINT 1 - Finishing structure and routes

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -4,17 +4,17 @@ import SidePanel from '../../components/SidePanel';
 import UserPanel from '../../components/UserPanel';
 import Main from '../../components/Main';
 
-const Dashboard = () => {
+const Products = () => {
   return (
     <div className="global-container">
       <SidePanel>
         <UserPanel />
       </SidePanel>
       <Main>
-        <h1>User Dashboard</h1>
+        <h1>Products</h1>
       </Main>
     </div>
   )
 };
 
-export default Dashboard;
+export default Products;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
+import Products from '../pages/Products';
+import Partners from '../pages/Partners';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Dashboard} />
+    <Route exact path="/products" component={Products} />
+    <Route exact path="/partners" component={Partners} />
   </Switch>
 );
 
