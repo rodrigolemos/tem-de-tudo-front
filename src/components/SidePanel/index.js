b94a6f90c20ec64e6e976df2f710cd6046fc4f7c
Rodrigo Lemos <rodrigolemosrl@hotmail.com>
2020-10-22 12:41:23 -0300

    Adding simple navbar

--- src/components/SidePanel/index.js
+++ src/components/SidePanel/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BsCloudFill } from 'react-icons/bs';
 import { GiPencilBrush } from 'react-icons/gi';
-import { Container } from './styles';
+import { Container, SideNav } from './styles';
 
 const SidePanel = ({ children }) => {
+
+  const [isShown, setShow] = useState('N');
+
+  const toggleNav = () => {
+    setShow(isShown === 'S' ? 'N' : 'S');
+  }
+
   return (
     <Container>
-      <GiPencilBrush className="bg-img" />
-      {children}
+      <div className="desktop">
+        <GiPencilBrush className="bg-img" />
+        {children}
+      </div>
+      <div className="mobile">
+        <BsCloudFill className="icon-open" title="Menu" onClick={toggleNav} />
+        <SideNav show={isShown}>
+          <span className="closebtn" onClick={toggleNav} >&times;</span>
+          {children}
+        </SideNav>
+      </div>
     </Container>
   )
 }
--- src/components/SidePanel/styles.js
+++ src/components/SidePanel/styles.js
@@ -1,27 +1,73 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.aside`
-  height: 95vh;
-  width: 25vw;
-  display: flex;
-  justify-content: flex-start;
-  align-items: flex-start;
-  flex-direction: column;
-  background: var(--bg-primary);
-  color: var(--font-secondary);
-  position: relative;
+  .desktop {
+    height: 95vh;
+    width: 25vw;
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    flex-direction: column;
+    background: var(--bg-primary);
+    color: var(--font-secondary);
+    position: relative;
 
-  @media only screen and (max-width: 1070px) {
-    display: none;
+    @media only screen and (max-width: 1070px) {
+      display: none;
+    }
+
+    .bg-img {
+      position: absolute;
+      top: 0px;
+      right: -15rem;
+      width: 40rem;
+      height: 40rem;
+      color: var(--bg-primary-dark);
+      z-index: 0;
+    }
   }
 
-  .bg-img {
+  .mobile {
+    color: #EEE;
+
+    .icon-open {
+      z-index: 1;
+      position: fixed;
+      color: var(--bg-primary);
+      top: 1rem;
+      left: 1.5rem;
+      font-size: 4rem;
+      cursor: pointer;
+    }
+
+    @media only screen and (min-width: 1070px) {
+      display: none;
+    }
+  }
+`;
+
+export const SideNav = styled.nav`
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: var(--bg-primary);
+  width: 100%;
+  display: flex;
+
+  ${({ show }) => show === 'N' && css`
+    display: none;
+  `}
+
+  .closebtn {
     position: absolute;
-    top: 0px;
-    right: -15rem;
-    width: 40rem;
-    height: 40rem;
-    color: var(--bg-primary-dark);
-    z-index: 0;
+    z-index: 2;
+    top: 0;
+    right: 2.5rem;
+    font-size: 3.6rem;
+    margin-left: 5rem;
+    cursor: pointer;
   }
 `;
