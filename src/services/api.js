41833f966d1a627736950110193a084681d7399d
Rodrigo <rodrigolemosrl@hotmail.com>
2020-10-21 21:15:22 -0300

    Environment

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
+const { REACT_APP_API_URL } = process.env;
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: REACT_APP_API_URL,
   timeout: 5000
 });
